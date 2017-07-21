7ffffe890c810d77f95925225239684a8921fae1
Alex Penman <apenman427@gmail.com>
2017-07-20 17:08:12 -0700

    Lazy load images

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,25 @@
-import $ from 'jquery';
-window.$ = window.jQuery = $; // Making these bad boys global so we dont require them anywhere else
-
+// import $ from 'jquery';
+// window.$ = window.jQuery = $; // Making these bad boys global so we dont require them anywhere else
 import Example from './components/_Example.js';
 
 (function($) {
-
   var App =  {
     init: function() {
         Example();
+    },
+    initAppearWatchers: function() {
+      console.log("testing");
+      appear({
+        elements: function elements() {
+          return document.getElementsByClassName('appear');
+        },
+        appear: function appear(el) {
+          var item = $(el);
+          item.addClass('visible-element');
+        },
+        bounds: 0
+      })
     }
   }
-
-  $(document).ready(function() { App.init(); });
+  $(document).ready(function() { App.init(); App.initAppearWatchers(); $("img.lazy").lazyload({});});
 })(jQuery);
